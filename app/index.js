d48cfe056c40c336deee4b0d40988cc1d46c0a4b
luksvrd <ljvy9b@gmail.com>
2022-12-15 19:40:20 -0600

    Basic html & json resopnses

--- app/index.js
+++ app/index.js
@@ -5,17 +5,23 @@ const app = express();
 // Specify the port to listen on, 3000 is the default. A port is a way to identify a specific process on a computer
 const port = 3001;
 
-// We want our express app to use the stuff in 'public' as statci assets
-// Satic assets are the same for all visitors - nothing personalized or dynamic
-// when working with routing, order matters - the first route that matches will be used
-app.use(express.static("public"));
+// This is a route, it's a way to define a path that the server will respond to
+app.get("/", (req, res) => {
+  // This is the response, it's what the server will send back to the client
+  res.send(
+    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
+  );
+});
 
-// Since theres no 'hello.html' file, we'll send back some HTML
-app.use("/hello", (_, res) => res.send("<p>Hello World!</p>"));
+app.get("/api", (req, res) => {
+  // This is the most modern use of JSON, but it's not supported by IE
+  // .Json is basically a way to send data to the client in a way that is easy to parse
+  // res.json() is a method that will send the data as JSON, otherwise it will send it as a string
+  res.json({
+    term: "api",
+    description:
+      "An application programming interface, is a computing interface that defines interactions between multiple software intermediaries",
+  });
+});
 
-// The '_' is a placeholder for the request object, we're only interested in the response object
-// The response object is what we send back to the client
-// app.get("/", (_, res) => res.send("<h1>Navigate to /send or /routes</h1>"));
-
-// Start the server
 app.listen(port, () => console.log(`Example app listening on port 3001!`));
