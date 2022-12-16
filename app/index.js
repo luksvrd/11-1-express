2d9d4456e65b700d23b6846e1bb6a942fe7adcc0
luksvrd <ljvy9b@gmail.com>
2022-12-15 21:26:13 -0600

    Allow CORS from Vite front-end

--- app/index.js
+++ app/index.js
@@ -1,17 +1,12 @@
-// express is a web framework for Node.js that makes it easy to create a web server. It's a way to create a server that can respond to requests
+import cors from "cors";
 import express from "express";
-// Must specify this is JSON otherwise it will be treated as a string because it's not a .js file
 import termsData from "./db/terms.json" assert { type: "json" };
-// Terms data is an array and because its an array we  can use the 'find' method to find a specific term. And we can compare that term to the term we are looking for.
 
-// Start Express
+// Start express
 const app = express();
-// Specify the port to listen on, 3000 is the default. A port is a way to identify a specific process on a computer
 const port = 3001;
 
-// TODO: Use Query Parameters to allow an option to SORT the terms asending or descending
-// this is a file that will be served up when the user goes to the root of the site.
-app.get("/api/terms", (req, res) => {
+app.get("/api/terms", cors({ origin: "http://localhost:5173" }), (req, res) => {
   // 'req.query' is an object that comes from the '?student=josh'
   // This will be either 'asc' or 'desc'
   const { sort } = req.query;
@@ -20,13 +15,9 @@ app.get("/api/terms", (req, res) => {
 
   switch (sort) {
     case "asc":
-      // a.term.localeCompare(b.term) will return a number. If a is greater than b, it will return a positive number. If a is less than b, it will return a negative number. If they are equal, it will return 0.
-      // this is sorting a to z
       ret = termsData.sort((a, b) => a.term.localeCompare(b.term));
       break;
     case "desc":
-      // localeCompare is a method that will compare two strings and return a number. If the first string is greater than the second, it will return a positive number. If the first string is less than the second, it will return a negative number. If they are equal, it will return 0.
-      // This is sorting z to a
       ret = termsData.sort((a, b) => b.term.localeCompare(a.term));
       break;
     default:
