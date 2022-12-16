543b13825ff673e14592950130569bc19ec6c903
luksvrd <ljvy9b@gmail.com>
2022-12-15 19:41:45 -0600

    refactor: underscores show no request parameters

--- app/index.js
+++ app/index.js
@@ -6,14 +6,14 @@ const app = express();
 const port = 3001;
 
 // This is a route, it's a way to define a path that the server will respond to
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   // This is the response, it's what the server will send back to the client
   res.send(
     `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
   );
 });
 
-app.get("/api", (req, res) => {
+app.get("/api", (_, res) => {
   // This is the most modern use of JSON, but it's not supported by IE
   // .Json is basically a way to send data to the client in a way that is easy to parse
   // res.json() is a method that will send the data as JSON, otherwise it will send it as a string
